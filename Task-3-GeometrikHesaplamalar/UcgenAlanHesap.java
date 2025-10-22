0bf9c5055fc31dfb725f3fce4766a43b7e882bce
Amine Ceren Yiğit <aminecyigit@gmail.com>
2025-10-22 23:33:12 +0300

    Add UcgenAlanHesap class to calculate triangle area

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,9 @@
+public class UcgenAlanHesap {
+    public static void main(String[] args) {
+        double kenara = 3;
+        double kenarb = 4;
+        double kenarc = 5;
+        double alan = Math.sqrt(6*(6-3)*(6-4)*(6-5));
+        System.out.println("Alan = " + alan);
+    }
+}
