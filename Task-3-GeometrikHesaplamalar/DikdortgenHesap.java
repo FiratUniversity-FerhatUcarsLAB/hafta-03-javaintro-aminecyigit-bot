d973984baf3963b3dcec5514b4ea1d5d801eac6f
Amine Ceren Yiğit <aminecyigit@gmail.com>
2025-10-22 23:01:16 +0300

    Add DikdörtgenHesap class for rectangle calculations

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,10 @@
+public class DikdörtgenHesap {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+        double cevre = 2*(4.5 + 7.9);
+        double alan = 4.5*7.9;
+        System.out.println("Alan = " + alan );
+        System.out.println( "Cevre="  + cevre );
+    }
+}
