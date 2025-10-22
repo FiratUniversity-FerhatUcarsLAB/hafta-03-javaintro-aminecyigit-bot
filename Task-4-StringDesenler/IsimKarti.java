4ca4495b8825dedde0165fad46bb0517863d8b55
Amine Ceren Yiğit <aminecyigit@gmail.com>
2025-10-22 23:44:16 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,15 @@
+public class IsımKarti {
+    public static void main(String[] args) {
+        String ad = "Amine Ceren";
+        String soyad = "Yiğit";
+        String ogrNo = "250541048";
+        String bolum = "Yazılım Müh.";
+        System.out.println("===================");
+        System.out.println(" OGRENCI BILGILERI ");
+        System.out.println("Ad :" + ad);
+        System.out.println("Soyad :" + soyad);
+        System.out.println("No :" + ogrNo);
+        System.out.println("Bolum :" + bolum);
+        System.out.println("===================");
+    }
+}
