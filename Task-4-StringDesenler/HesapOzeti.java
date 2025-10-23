1ade1f7bc28a0bcf35463a8d10ea980e926719a3
Amine Ceren Yiğit <aminecyigit@gmail.com>
2025-10-23 13:37:59 +0300

    Add HesapOzet class for invoice summary display

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,23 @@
+public class HesapOzet {
+    public  static void main(String[] args) {
+        System.out.println("------------ HESAP ÖZETİ ------------");
+        System.out.println("Ürün Adı     Miktar      Birim Fiyat (TL)");
+        System.out.println("Toplam Tutar (TL)                        ");
+        System.out.println("-----------------------------------------");
+        System.out.println("-------------------");
+        System.out.println("Elma           3            15.50");
+        System.out.println("46.50");
+        System.out.println("Ekmek          2            10.00");
+        System.out.println("20.00");
+        System.out.println("Süt            1            32.75");
+        System.out.println("32.75");
+        System.out.println("Çikolata       5             8.25");
+        System.out.println("41.25");
+        System.out.println("-----------------------------------------");
+        System.out.println("--------------------");
+        System.out.println("                            GENEL TOPLAM:");
+        System.out.println("140.50");
+        System.out.println("-----------------------------------------");
+        System.out.println("--------------------");
+    }
+}
